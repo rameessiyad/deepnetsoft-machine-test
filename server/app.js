c6c5beecb36c642b049460e6e12dce5200c618ee
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-12-12 11:33:31 +0530

    server setup done

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

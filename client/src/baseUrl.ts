b586051d671884b3b4ba641d64fbe5f45c0df491
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-12-13 07:34:04 +0530

    frontend builded

--- client/src/baseUrl.ts
+++ client/src/baseUrl.ts
@@ -1,1 +1,2 @@
-export const baseUrl = "http://localhost:3000/api/v1";
+// export const baseUrl = "http://localhost:3000/api/v1";
+export const baseUrl = "https://deepnetsoft-machine-test.onrender.com/api/v1";
